ebaf99f7e6ed755c6717af60902c8c2a9a43bc31
Kazuki Tobita <kazukiti201@gmail.com>
2018-12-01 05:01:30 +0900

    とりあえず動いたゾ

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="hello">
     <header>
-      <h1>{{ msg }}</h1>
+      <h1>{{ appName }}</h1>
     </header>
     <h3>上の段</h3>
     <div class="input_text">
-      <input class="form-control" id="title_top" value="タイトル">
+      <input class="form-control" id="title_top" v-model="titleTop">
     </div>
     <h3>下の段</h3>
     <div class="input_text">
-      <input class="form-control" id="title_bottom" value="ジェネレーターだゾ">
+      <input class="form-control" id="title_bottom" v-model="titleBottom">
     </div>
     <div class="row">
       <div class="col-sm-6">
         <p>左側</p>
-        <input type="text" class="form-control" id="left_cap" value="脚本 : フロント 太郎">
+        <input type="text" class="form-control" id="left_cap" v-model="leftCap">
       </div>
       <div class="col-sm-6">
         <p>左側</p>
-        <input type="text" class="form-control" id="right_cap" value="監督 : エンド 次郎">
+        <input type="text" class="form-control" id="right_cap" v-model="rightCap">
       </div>
     </div>
     <div class="submit">
-      <button type="button" class="btn btn-success" id="enter">嵐を呼ぶ!</button>
+      <button type="button" class="btn btn-success" id="enter" @click="generate">嵐を呼ぶ!</button>
       <button type="button" class="btn btn-success" id="tweet">嵐をシェアする!</button>
       <a href="https://twitter.com/intent/tweet?&hashtags=嵐を呼ぶジェネレータ&url=https://afterschoolstudy.github.io/generator_of_call_a_storm/dist/" class="twitter-hashtag-button" data-show-count="false">Tweet #嵐を呼ぶジェネレータ</a>
     </div>
@@ -31,80 +31,80 @@
       <img id="output_image" />
     </div>
     <footer>
-      <p>poewred by ITCreateClub</p>
+      <p>powered by ITCreateClub</p>
     </footer>
   </div>
 </template>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Generator',
   data () {
     return {
-      msg: '嵐を呼ぶサブタイトルジェネレータ'
+      appName: '嵐を呼ぶサブタイトルジェネレータ',
+      titleTop: 'タイトル',
+      titleBottom: 'ジェネレーターだゾ',
+      leftCap: '脚本 : フロント 太郎',
+      rightCap: '監督 : エンド 次郎'
+    }
+  },
+  methods: {
+    generate () {
+      this.drawBg()
+      let ctx = document.getElementById('output_field').getContext('2d')
+      ctx.clearRect(0, 0, 2880, 1620)
+      this.drawBg()
+
+      // 嵐部分
+      ctx.font = '250px sans-serif'
+      ctx.textAlign = 'left'
+      let topText = this.titleTop
+      let bottomText = this.titleBottom
+      ctx.fillStyle = '#5853A0'
+      ctx.fillText(topText, 140, 590)
+      ctx.textAlign = 'right'
+      ctx.fillText(bottomText, 2740, 930)
+
+      // 左キャプション
+      let leftCap = this.leftCap
+      ctx.textAlign = 'left'
+      ctx.lineWidth = 5
+      ctx.fillStyle = '#FFF'
+      ctx.font = '90px cursive'
+      ctx.fillText(leftCap, 400, 1350)
+      ctx.strokeStyle = 'gray'
+      ctx.strokeText(leftCap, 400, 1350)
+
+      // 右キャプション
+      let rightCap = this.rightCap
+      ctx.textAlign = 'right'
+      ctx.lineWidth = 5
+      ctx.fillStyle = '#FFF'
+      ctx.font = '90px cursive'
+      ctx.fillText(rightCap, 2480, 1350)
+      ctx.strokeStyle = 'gray'
+      ctx.strokeText(rightCap, 2480, 1350)
+
+      // 生成
+      ctx = document.getElementById('output_field')
+      let png = ctx.toDataURL()
+      document.getElementById('output_image').src = png
+      $('#output').css('border', '0')
+      $('#output_field').hide()
+      $('#output_image').show()
+    },
+    drawBg () {
+      let ctxBase = document.getElementById('output_field').getContext('2d')
+      ctxBase.beginPath()
+      let ctxBaseColor = ctxBase.createLinearGradient(0, 410, 0, 1620)
+      ctxBaseColor.addColorStop(0.0, '#FD81A6')
+      ctxBaseColor.addColorStop(1.0, '#FFFF83')
+      ctxBase.fillStyle = ctxBaseColor
+      ctxBase.fillRect(0, 0, 2880, 1620)
     }
   }
 }
-$(function(){
-  // 背景グラデ
-  fillBack();
-
-  $('#enter').click(function(){
-    var ctx = document.getElementById("output_field").getContext("2d");
-    ctx.clearRect(0, 0, 2880, 1620);
-    fillBack();
-    // 嵐部分
-    ctx.font = '250px sans-serif';
-    ctx.textAlign = 'left';
-    var top_text = $('#title_top').val();
-    var bottom_text = $('#title_bottom').val();
-    ctx.fillStyle = '#5853A0';
-    ctx.fillText(top_text,140,590);
-    ctx.textAlign = 'right';
-    ctx.fillText(bottom_text,2740,930);
-
-    // 左キャプション
-    var left_cap = $('#left_cap').val();
-    ctx.textAlign = 'left';
-    ctx.lineWidth = 5;
-    ctx.fillStyle = "#FFF";
-    ctx.font = "90px cursive";
-    ctx.fillText(left_cap, 400, 1350);
-    ctx.strokeStyle = "gray";
-    ctx.strokeText(left_cap, 400, 1350);
-
-    // 右キャプション
-    var right_cap = $('#right_cap').val();
-    ctx.textAlign = 'right';
-    ctx.lineWidth = 5;
-    ctx.fillStyle = "#FFF";
-    ctx.font = "90px cursive";
-    ctx.fillText(right_cap, 2480, 1350);
-    ctx.strokeStyle = "gray";
-    ctx.strokeText(right_cap, 2480, 1350);
-
-    // 生成
-    var ctx = document.getElementById("output_field");
-    var png = ctx.toDataURL();
-    document.getElementById("output_image").src = png;
-    $('#output').css('border', '0');
-    $('#output_field').hide();
-    $('#output_image').show();
-  });
-});
-function fillBack(){
-  // 背景グラデ
-  var ctx_base = document.getElementById("output_field").getContext("2d");
-  ctx_base.beginPath();
-  var ctx_base_color = ctx_base.createLinearGradient(0, 410, 0, 1620);
-  ctx_base_color.addColorStop(0.0, '#FD81A6');
-  ctx_base_color.addColorStop(1.0, '#FFFF83');
-  ctx_base.fillStyle = ctx_base_color;
-  ctx_base.fillRect(0, 0, 2880, 1620);
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -186,7 +186,6 @@ h3{
 .output #output_field{
   width: 100%;
   height: 100%;
-  /*background: linear-gradient(0deg,#FFFF83,#FD81A6);*/
   font-size: 50px;
 }
 .output #output_image{
